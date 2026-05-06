c18d5cb71a06e024554c1405e7e5ff2bdcebf451
Steiynbrodt <180658853+Steiynbrodt@users.noreply.github.com>
2026-05-06 20:05:53 +0200

    Increase next round delay by 2 seconds

--- main.py
+++ main.py
@@ -521,7 +521,7 @@ class SmashOrPassApp:
             self._receive_vote_results(results_payload)
             if self.next_round_after_id:
                 self.root.after_cancel(self.next_round_after_id)
-            self.next_round_after_id = self.root.after(1500, self.next_image)
+            self.next_round_after_id = self.root.after(3500, self.next_image)
 
     def receive_vote_results(self, data, addr=None):
         self.root.after(0, lambda: self._receive_vote_results(data))
